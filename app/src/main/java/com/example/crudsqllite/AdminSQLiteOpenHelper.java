3c01565e9b48f03be5660a9bbf9f9586e40e036c
Oscar Salazar <salazaroscar9705@gmail.com>
2020-11-09 16:38:23 -0500

    crud de estudiantes y cursos

--- app/src/main/java/com/example/crudsqllite/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/crudsqllite/AdminSQLiteOpenHelper.java
@@ -9,16 +9,16 @@ import androidx.annotation.Nullable;
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table estudiantes (cedula_est integer primary key, nombre_est text, id_curso_est)");
-        db.execSQL("create table cursos (codigo_cu integer primary key, nombre_cu text, credito_cu integer)");
+        db.execSQL("create table estudiantes (cedula_est integer primary key, nombre_est text, edad_est integer, id_curso integer)");
+        db.execSQL("create table cursos (codigo_cu integer primary key AUTOINCREMENT, nombre_cu text, creditos_cu integer)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("drop table if exists estudiantes");
         db.execSQL("drop table if exists cursos");
-        db.execSQL("create table estudiantes (cedula_est integer primary key, nombre_est text, id_curso_est)");
-        db.execSQL("create table cursos (codigo_cu integer primary key, nombre_cu text, credito_cu integer)");
+        db.execSQL("create table estudiantes (cedula_est integer primary key, nombre_est text, edad_est integer, id_curso integer)");
+        db.execSQL("create table cursos (codigo_cu integer primary key AUTOINCREMENT, nombre_cu text, creditos_cu integer)");
     }
 
     public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
--- app/src/main/java/com/example/crudsqllite/MainActivity.java
+++ app/src/main/java/com/example/crudsqllite/MainActivity.java
@@ -7,11 +7,18 @@ import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
-    EditText et1, et2, et3, et4;
+    EditText et1, et2, et3, et4, et5, et6;
+    Spinner sp;
+    List<String> cursos;
+    List<Integer> cursosId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,42 +26,105 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         et1 = (EditText) findViewById(R.id.editText); //cedula
         et2 = (EditText) findViewById(R.id.editText2); //nombre
-        et3 = (EditText) findViewById(R.id.editText3); //colegio
-        et4 = (EditText) findViewById(R.id.editText4); //salon
+        et3 = (EditText) findViewById(R.id.editText3); //edad
+
+        et4 = (EditText) findViewById(R.id.editText4); // nombre del curso
+        et5 = (EditText) findViewById(R.id.editText5); // creditos del curso
+        et6 = (EditText) findViewById(R.id.editText6); // codigo del curso
+
+        sp= (Spinner)findViewById(R.id.spinner); // listado de cursos
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, cursos);
+        sp.setAdapter(adapter);
+
+
+    }
+    public void listarCursos(){
+
+
+        AdminSQLiteOpenHelper   admin = new AdminSQLiteOpenHelper(this,"administracion",null, 1);
+        SQLiteDatabase  bd = admin.getWritableDatabase();
+        Cursor fila = bd.rawQuery("select codigo_cu ,nombre_cu from cursos ", null);
+        while(fila.moveToNext()){
+            cursosId.add(fila.getInt(0));
+            cursos.add(fila.getString(1));
+        }
+
     }
 
     public void insertar(View v) {
         AdminSQLiteOpenHelper   admin = new AdminSQLiteOpenHelper(this,"administracion",null, 1);
         SQLiteDatabase  bd = admin.getWritableDatabase();
+
+        int idSpinner  = (int) sp.getSelectedItemId();
+
         String cedula = et1.getText().toString();
         String nombre = et2.getText().toString();
-        String colegio = et3.getText().toString();
-        String salon = et4.getText().toString();
+        String edad = et3.getText().toString();
+        int idCurso = cursosId.get(idSpinner-1);
+
         ContentValues registro = new ContentValues();
-        registro.put("cedula_vt", cedula);
-        registro.put("nombre_vt", nombre);
-        registro.put("colegio_vt", colegio);
-        registro.put("salon_vt", salon);
-        bd.insert("votantes", null, registro);
+        registro.put("cedula_est", cedula);
+        registro.put("nombre_est", nombre);
+        registro.put("edad_est", edad);
+        registro.put("id_curso", idCurso);
+        bd.insert("estudiantes", null, registro);
         bd.close();
         et1.setText("");
         et2.setText("");
         et3.setText("");
         et4.setText("");
-        Toast.makeText(this, "se cargaron los datos de la persona", Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, "se cargaron los datos del estudiante", Toast.LENGTH_SHORT).show();
+    }
+
+    public void insertarCurso(View v) {
+        AdminSQLiteOpenHelper   admin = new AdminSQLiteOpenHelper(this,"administracion",null, 1);
+        SQLiteDatabase  bd = admin.getWritableDatabase();
+
+        String nombre = et4.getText().toString();
+        String credito = et5.getText().toString();
+
+        ContentValues registro = new ContentValues();
+
+        registro.put("nombre_cu", nombre);
+        registro.put("creditos_cu", credito);
+
+        bd.insert("cursos", null, registro);
+        bd.close();
+        listarCursos();
+        et1.setText("");
+        et2.setText("");
+        et3.setText("");
+        et4.setText("");
+        Toast.makeText(this, "se cargaron los datos del curso", Toast.LENGTH_SHORT).show();
     }
 
     public void consultar(View v) {
         AdminSQLiteOpenHelper   admin = new AdminSQLiteOpenHelper(this,"administracion",null, 1);
         SQLiteDatabase  bd = admin.getWritableDatabase();
         String cedula = et1.getText().toString();
-        Cursor fila = bd.rawQuery("select nombre_vt, colegio_vt, salon_vt from votantes where cedula_vt =" +cedula,null);
+        Cursor fila = bd.rawQuery("select nombre_est, edad_est from estudiantes where cedula_est =" +cedula,null);
         if(fila.moveToFirst()){
             et2.setText(fila.getString(0));
             et3.setText(fila.getString(1));
-            et4.setText(fila.getString(2));
         } else {
-            Toast.makeText(this, "No existe una persona con dicha cedula ", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "No existe un estudiante con dicha cedula ", Toast.LENGTH_SHORT).show();
+        }
+
+        bd.close();
+        listarCursos();
+    }
+    public void consultarCurso(View v) {
+        AdminSQLiteOpenHelper   admin = new AdminSQLiteOpenHelper(this,"administracion",null, 1);
+        SQLiteDatabase  bd = admin.getWritableDatabase();
+        String id = et6.getText().toString();
+        Cursor fila = bd.rawQuery("select nombre_cu, creditos_cu from cursos where codigo_cu =" +id,null);
+        if(fila.moveToFirst()){
+            et4.setText(fila.getString(0));
+            et5.setText(fila.getString(1));
+
+        } else {
+            Toast.makeText(this, "No existe un curso con ese id ", Toast.LENGTH_SHORT).show();
         }
         bd.close();
     }
@@ -64,19 +134,17 @@ public class MainActivity extends AppCompatActivity {
         SQLiteDatabase  bd = admin.getWritableDatabase();
         String cedula = et1.getText().toString();
         String nombre = et2.getText().toString();
-        String colegio = et3.getText().toString();
-        String salon = et4.getText().toString();
+        String edad = et3.getText().toString();
         ContentValues registro = new ContentValues();
-        registro.put("nombre_vt", nombre);
-        registro.put("colegio_vt", colegio);
-        registro.put("salon_vt", salon);
-        bd.insert("votantes", null, registro);
-        int cant = bd.update("votantes", registro, "cedula_vt="+cedula,null);
+        registro.put("nombre_est", nombre);
+        registro.put("edad_est", edad);
+        bd.insert("estudiantes", null, registro);
+        int cant = bd.update("estudiantes", registro, "estudiante_st="+cedula,null);
         bd.close();
         if( cant == 1){
-            Toast.makeText(this, "se modificaron los datos de la persona", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "se modificaron los datos del estudiante", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "no existe una persona con dicha cedula", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "no existe un estudiante con esa cedula", Toast.LENGTH_SHORT).show();
         }
         et1.setText("");
         et2.setText("");
@@ -84,16 +152,49 @@ public class MainActivity extends AppCompatActivity {
         et4.setText("");
 
     }
+    public void actualizarCurso(View v) {
+        AdminSQLiteOpenHelper   admin = new AdminSQLiteOpenHelper(this,"administracion",null, 1);
+        SQLiteDatabase  bd = admin.getWritableDatabase();
+        String codigo = et6.getText().toString();
+        String nombre = et4.getText().toString();
+        String creditos = et5.getText().toString();
+        ContentValues registro = new ContentValues();
+        registro.put("nombre_cu", nombre);
+        registro.put("creditos_cu", creditos);
+        bd.insert("cursos", null, registro);
+        int cant = bd.update("cursos", registro, "codigo_cu="+codigo,null);
+        bd.close();
+        if( cant == 1){
+            Toast.makeText(this, "se modificaron los datos del curso", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "no existe un curso con ese id", Toast.LENGTH_SHORT).show();
+        }
+        et4.setText("");
+        et5.setText("");
+        et6.setText("");
+
+    }
 
     public void eliminar(View v) {
         AdminSQLiteOpenHelper   admin = new AdminSQLiteOpenHelper(this,"administracion",null, 1);
         SQLiteDatabase  bd = admin.getWritableDatabase();
         String cedula = et1.getText().toString();
-        int cant = bd.delete("votantes","cedula_vt = "+cedula, null);
+        int cant = bd.delete("estudiantes","cedula_est = "+cedula, null);
         if(cant == 1){
             Toast.makeText(this, "se eliminó correctamente", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(this, "no existe una persona con dicha cedula", Toast.LENGTH_SHORT).show();
+            Toast.makeText(this, "no existe un estudiante con dicha cedula", Toast.LENGTH_SHORT).show();
+        }
+    }
+    public void eliminarCurso(View v) {
+        AdminSQLiteOpenHelper   admin = new AdminSQLiteOpenHelper(this,"administracion",null, 1);
+        SQLiteDatabase  bd = admin.getWritableDatabase();
+        String id = et6.getText().toString();
+        int cant = bd.delete("cursos","codigo_cu = " + id, null);
+        if(cant == 1){
+            Toast.makeText(this, "se eliminó correctamente", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(this, "no existe un curso con dicho codigo", Toast.LENGTH_SHORT).show();
         }
     }
 
