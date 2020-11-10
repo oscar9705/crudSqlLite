382355450feb577b83b3d81e4724b1acdc7addf1
Oscar Salazar <salazaroscar9705@gmail.com>
2020-11-10 11:47:49 -0500

    tablas de empleados y empresas

--- app/src/main/java/com/example/crudsqllite/AdminSQLiteOpenHelper.java
+++ app/src/main/java/com/example/crudsqllite/AdminSQLiteOpenHelper.java
@@ -9,16 +9,16 @@ import androidx.annotation.Nullable;
 public class AdminSQLiteOpenHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table estudiantes (cedula_est integer primary key, nombre_est text, edad_est integer, id_curso integer)");
-        db.execSQL("create table cursos (codigo_cu integer primary key AUTOINCREMENT, nombre_cu text, creditos_cu integer)");
+        db.execSQL("create table empleados (id_empleado integer primary key, nombre_empleado text,correo_empleado text, id_empresa integer)");
+        db.execSQL("create table empresas (id_empresa integer primary key AUTOINCREMENT, nombre_empresa text, direccion_empresa text)");
     }
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        db.execSQL("drop table if exists estudiantes");
-        db.execSQL("drop table if exists cursos");
-        db.execSQL("create table estudiantes (cedula_est integer primary key, nombre_est text, edad_est integer, id_curso integer)");
-        db.execSQL("create table cursos (codigo_cu integer primary key AUTOINCREMENT, nombre_cu text, creditos_cu integer)");
+        db.execSQL("drop table if exists empresas");
+        db.execSQL("drop table if exists empleados");
+        db.execSQL("create table empleados (id_empleado integer primary key, nombre_empleado text,correo_empleado text, id_empresa integer)");
+        db.execSQL("create table empresas (id_empresa integer primary key AUTOINCREMENT, nombre_empresa text, direccion_empresa text)");
     }
 
     public AdminSQLiteOpenHelper(Context context, String name, SQLiteDatabase.CursorFactory factory, int version) {
